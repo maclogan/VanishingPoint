fc74f713d689e2d54af58f8b406227b603d1b6aa
maclogan <p13mlogan@hotmail.com>
2024-08-17 23:00:08 -0600

    Got scaling working, added basic tree generation

--- scripts/character.gd
+++ scripts/character.gd
@@ -4,6 +4,19 @@ extends CharacterBody2D
 @export var friction = 0.9
 @export var acceleration = 0.9
 
+@export var shrink_time: int = 600
+
+@onready var camera_2d: Camera2D = $Camera2D
+@onready var shrink_timer: Timer = $ShrinkTimer
+@onready var scale_bar: ProgressBar = $CanvasLayer/ScaleBar
+
+var initial_scale = 1
+@export var initial_zoom = 2
+
+func _ready() -> void:
+	shrink_timer.wait_time = shrink_time
+	shrink_timer.start()
+
 func get_input():
 	var input = Vector2()
 	if Input.is_action_pressed('right'):
@@ -17,6 +30,10 @@ func get_input():
 	return input
 
 func _physics_process(delta):
+	var scale_factor = shrink_timer.time_left / shrink_time
+	set_scale(Vector2(initial_scale * scale_factor, initial_scale * scale_factor))
+	camera_2d.set_zoom(Vector2(initial_zoom / scale_factor, initial_zoom / scale_factor))
+	scale_bar.value = 100 * scale_factor
 	var direction = get_input()
 	if direction.length() > 0:
 		velocity = velocity.lerp(direction.normalized() * speed, acceleration)
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,3 +1,39 @@
 extends Node2D
 
-var grassAtlas = Vector2i(0,0)
+@onready var tile_map: TileMap = $TileMap
+@export var noise_tree_texture: NoiseTexture2D
+var tree_noise: Noise
+var map_noise: Noise
+var tree_atlas = [Vector2i(1,1)] 
+var width = 100
+var height = 100
+@onready var noise_generator: NoiseGenerator = $NoiseGenerator
+
+var tree_tiles_arr = []
+var count = 0
+var noise_val_arr = []
+			
+func generate_resources() -> void:
+	for x in range(width):
+		for y in range(height):
+			var tree_noise_val: float = tree_noise.get_noise_2d(x, y)
+			var map_noise_val: float = map_noise.get_noise_2d(x, y)
+			noise_val_arr.append(tree_noise_val)
+			if map_noise_val >= 0.2 and tree_noise_val >= 0.4:
+				count = count + 1
+				tile_map.set_cell(2, Vector2i(x, y), 2, Vector2i(1,1))
+	print(count, noise_val_arr.max(), noise_val_arr.min())
+
+
+func _on_noise_generator_generation_finished() -> void:
+	map_noise = noise_generator.settings.noise
+	noise_tree_texture.noise.seed = map_noise.seed
+	print (map_noise.seed, noise_tree_texture.noise.seed)
+	tree_noise = noise_tree_texture.noise
+	generate_resources()
+
+
+func _on_noise_generator_chunk_generation_finished(chunk_position: Vector2i) -> void:
+#	Move asset generation here, use chunk size and position as width/height
+#	Will need to figure out a way to retain asset position on exit
+	pass # Replace with function body.
